121c098f763b2a0f70563c18610a54dcfd5a7184
Robert Matundu <matundukabamba@gmail.com>
2025-03-22 23:12:22 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="font-extrabold text-center text-7xl text-amber-700 bg-black py-3.5">Teste</h1>
+    </>
+  )
+}
+
+export default App
