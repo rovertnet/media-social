9f9a2a584a3220f2ebfce4f5cefdff0ba3bbb2f5
Robert Matundu <matundukabamba@gmail.com>
2025-03-24 12:42:03 +0100

    last commit

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -3,13 +3,7 @@ import React from 'react'
 export default function SignUp() {
   return (
     <>
-      <div className=" h-screen w-screen block">
-        <div className="bg-blue-200 px-10 py-10 rounded-xl h-screen mx-32 my-32">
-          <h1 className="font-extrabold text-3xl text-slate-900 ">
-            Inscrivez-vous
-          </h1>
-        </div>
-      </div>
+      <div className=""></div>
     </>
   );
 }
