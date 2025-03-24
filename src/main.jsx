4c23b46ba3d7bc6a555ff8c97515416429b93cb8
Robert Matundu <matundukabamba@gmail.com>
2025-03-24 12:05:09 +0100

    last commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Dashboard from "./pages/dashboard/Dashboard.jsx"
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Dashboard />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}>
+    </RouterProvider >
   </StrictMode>,
 )
--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <>
+      <div className=" h-screen "></div>
+    </>
   )
 }
