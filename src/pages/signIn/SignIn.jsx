8914bdccadf8f57909d87e8521483e9605431b78
Robert Matundu <matundukabamba@gmail.com>
2025-03-24 17:48:02 +0100

    last commit

--- src/pages/signIn/SignIn.jsx
+++ src/pages/signIn/SignIn.jsx
@@ -2,6 +2,92 @@ import React from 'react'
 
 export default function SignIn() {
   return (
-    <div>SignIn</div>
+    <>
+          <div className="bg-white px-10 py-10 rounded-md m-20 md:w-1/2 md:mt-20 md:mx-auto block">
+            <h1 className="text-2xl font-bold text-center">Inscription</h1>
+    
+            <p className="text-center text-gray-500 mt-2">
+              Créez un compte pour accéder à votre espace personnel
+            </p>
+    
+            <div className="flex justify-center items-center my-5 gap-8">
+              <div className="border-[1px] border-gray-300 rounded-md py-2 px-2">
+                <FaGoogle className="text-3xl text-blue-600 font-bold" />
+              </div>
+    
+              <div className="border-[1px] border-gray-300 rounded-md py-2 px-2">
+                <FaFacebook className="text-3xl text-blue-600 font-bold" />
+              </div>
+    
+              <div className="border-[1px] border-gray-300 rounded-md py-2 px-2">
+                <FaApple className="text-3xl text-black font-bold" />
+              </div>
+            </div>
+    
+            <form className="mt-5">
+              <div className="mb-4">
+                
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  placeholder="Nom d\'utilisateur"
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+              </div>
+    
+              <div className="mb-4">
+                
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Adresse email"
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+              </div>
+    
+              <div className="mb-4">
+                
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Mot de passe"
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+              </div>
+    
+              <div className="mb-4">
+                
+                <input
+                  type="password"
+                  name="password_confirmation"
+                  id="password_confirmation"
+                  placeholder="Confirmer le mot de passe"
+                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+              </div>
+    
+              <div className="flex items-center justify-between">
+                <button
+                  type="submit"
+                  className="bg-blue-500 hover:bg-blue-700 text-white cursor-pointer font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                >
+                  Connexion
+                </button>
+              </div>
+    
+              <div className="mt-5">
+                <p className="text-start text-gray-500">
+                  Vous avez déjà un compte ?{' '}
+                  <Link to="/connexion" className="text-blue-500">
+                    Inscrivez-vous
+                  </Link>
+                </p>
+              </div>
+            </form>
+          </div>
+        </>
   )
 }
--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -42,46 +42,34 @@ export default function SignUp() {
           </div>
 
           <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Adresse e-mail
-            </label>
+            
             <input
               type="email"
               name="email"
               id="email"
+              placeholder="Adresse email"
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
           </div>
 
           <div className="mb-4">
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Mot de passe
-            </label>
+            
             <input
               type="password"
               name="password"
               id="password"
+              placeholder="Mot de passe"
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
           </div>
 
           <div className="mb-4">
-            <label
-              htmlFor="password_confirmation"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Confirmation du mot de passe
-            </label>
+            
             <input
               type="password"
               name="password_confirmation"
               id="password_confirmation"
+              placeholder="Confirmer le mot de passe"
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
           </div>
