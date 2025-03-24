7c69582d850443f058f0c5a3568f9e2028077973
Robert Matundu <matundukabamba@gmail.com>
2025-03-24 12:52:16 +0100

    last commit

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 export default function SignUp() {
   return (
     <>
-      <div className=""></div>
+      <div className="bg-gray-200 h-screen max-h-screen flex justify-center items-center px-20 py-20">
+        <div className="bg-white px-10 py-10 rounded-md">
+          <h1 className="font-bold text-2xl text-slate-800">Inscrivez-vous</h1>
+
+          {/* médias sociaux */}
+          <div className="border-[1px] border-slate-500 rounded-md px-3.5 flex justify-center items-center">
+            
+          </div>
+          {/* formulaire */}
+          <form action="">
+
+          </form>
+        </div>
+      </div>
     </>
   );
 }
