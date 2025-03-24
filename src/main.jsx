1aa811db37852fe0ebe1915e545357a6b1e1248d
Robert Matundu <matundukabamba@gmail.com>
2025-03-24 12:08:00 +0100

    last commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Dashboard from "./pages/dashboard/Dashboard.jsx"
+import SignUp from './pages/signup/SignUp.jsx'
+import SignIn from './pages/signIn/SignIn.jsx'
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element: <Dashboard />
-  }
-])
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "/connexion",
+    element: <SignIn />,
+  },
+  {
+    path: "/inscription",
+    element: <SignUp />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
