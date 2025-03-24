65a829330a7efb90d345e785e4925fdd2c8ee9bd
Robert Matundu <matundukabamba@gmail.com>
2025-03-24 17:24:51 +0100

    last commit

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { FaGoogle } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaApple } from "react-icons/fa";
 
 
 export default function SignUp() {
@@ -18,7 +20,11 @@ export default function SignUp() {
           </div>
 
           <div className="border-[1px] border-gray-300 rounded-md py-2 px-2">
-            <FaGoogle className="text-3xl text-blue-600 font-bold" />
+            <FaFacebook className="text-3xl text-blue-600 font-bold" />
+          </div>
+
+          <div className="border-[1px] border-gray-300 rounded-md py-2 px-2">
+            <FaApple className="text-3xl text-blue-600 font-bold" />
           </div>
         </div>
 
