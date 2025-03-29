ebbf926504d9e9be8957c7f162dde1e77b305ffd
Robert Matundu <matundukabamba@gmail.com>
2025-03-29 23:43:34 +0100

    another commit

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -75,10 +75,10 @@ export default function Dashboard() {
           <Modal showModal={showModal} setShowModal={setShowModal} />
         </div>
 
-        <div className="flex flex-col gap-5 py-3.5 my-5 w-full mx-44">
-          <div className="flex justify-start flex-col items-center px-10 py-5  rounded-lg">
+        <div className="flex flex-col gap-5 ">
+          <div className="flex justify-start flex-col items-center px-10 py-3  rounded-lg">
             {posts.map((post) => (
-              <div className="bg-white w-full rounded-lg shadow-md my-10 mx-44">
+              <div className="bg-white w-full rounded-lg shadow-md my-5 mx-44">
                 <div key={post.id} className="flex flex-col gap-5 my-5 w-full ">
                   <div className="flex justify-start items-center gap-5 pl-5">
                     <FaUserCircle className="text-6xl font-extrabold text-gray-300" />
