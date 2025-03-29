de3213123ee7e00c96a6d52024ea82eca8d74976
Robert Matundu <matundukabamba@gmail.com>
2025-03-29 11:55:29 +0100

    another commit

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -10,7 +10,7 @@ export default function Dashboard() {
         <Sidebar />
         <div className="main-content flex-1 p-6 bg-gray-100">
           {session ? (
-            <div className="session-info flex space-x-3.5">
+            <div className="session-info flex space-x-3.5 justify-center items-center" >
               <h1 className="text-2xl font-bold mb-4">
                 Bonjour {session.username} !
               </h1>
