bb7923f2412441f55c77feb61165c921df83a0a5
Robert Matundu <matundukabamba@gmail.com>
2025-03-29 13:10:04 +0100

    another commit

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -9,7 +9,7 @@ export default function Dashboard() {
     <>
       <NavBars />
       <div className="flex h-screen">
-        <div className="main-content flex-1 p-6 bg-gray-100">
+        <div className="main-content flex-1 my-20 bg-gray-100">
           {session ? (
             <div className="session-info flex space-x-3.5 justify-center items-center">
               <h1 className="text-4xl font-extrabold mb-4">
