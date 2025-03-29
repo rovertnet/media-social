a637525a7aedca21f1782e435f6578b170f17a0e
Robert Matundu <matundukabamba@gmail.com>
2025-03-29 15:42:20 +0100

    another commit

--- src/pages/signIn/SignIn.jsx
+++ src/pages/signIn/SignIn.jsx
@@ -47,7 +47,7 @@ export default function SignIn() {
         </div>
 
         <div className="border-[1px] border-gray-300 rounded-md py-2 px-2">
-          <FaFacebook className="text-3xl text-blue-600 font-bold" />
+          <FaFacebook className="text-3xl text-red-500 font-bold" />
         </div>
 
         <div className="border-[1px] border-gray-300 rounded-md py-2 px-2">
--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -51,7 +51,7 @@ export default function SignUp() {
 
         <div className="flex justify-center items-center my-5 gap-8">
           <div className="border-[1px] border-gray-300 rounded-md py-2 px-2">
-            <FaGoogle className="text-3xl text-blue-600 font-bold" />
+            <FaGoogle className="text-3xl text-red-500 font-bold" />
           </div>
 
           <div className="border-[1px] border-gray-300 rounded-md py-2 px-2">
