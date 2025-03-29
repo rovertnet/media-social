acb1cf214b81660b3925e23baadafbd34fd00ec4
Robert Matundu <matundukabamba@gmail.com>
2025-03-29 18:24:15 +0100

    another commit

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -20,27 +20,24 @@ export default function Dashboard() {
   return (
     <>
       <NavBars />
-      
 
-      <div className="flex items-center justify-center bg-white mt-20">
-        <header className="">
-          <div className="flex justify-center items-center space-x-2.5">
-            <FaUserCircle className="text-4xl font-extrabold text-gray-300 " />
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              onClick={() => setShowModal(true)}
-            >
-              {session ? (
-                <>
-                  Quoi de neuf {session.username}
-                </>
-              ) : (
-                <p className="text-lg">Chargement de la session...</p>
-              )}
-            </button>
-          </div>
-        </header>
-        <Modal showModal={showModal} setShowModal={setShowModal} />
+      <div className="flex justify-center items-center  py-10 mt-20">
+        <div className="flex items-center justify-center bg-white mt-20">
+            <div className="flex justify-center items-center gap-5 px-10 py-5 bg-white rounded-lg">
+              <FaUserCircle className="text-4xl font-extrabold text-gray-300 " />
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                onClick={() => setShowModal(true)}
+              >
+                {session ? (
+                  <>Quoi de neuf {session.username}</>
+                ) : (
+                  <p className="text-lg">Chargement de la session...</p>
+                )}
+              </button>
+            </div>
+          <Modal showModal={showModal} setShowModal={setShowModal} />
+        </div>
       </div>
     </>
   );
