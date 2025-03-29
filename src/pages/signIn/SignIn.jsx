da02ac6d09cd95a280ce3095de6a85d8fa4b9d58
Robert Matundu <matundukabamba@gmail.com>
2025-03-29 16:05:34 +0100

    another commit

--- src/pages/signIn/SignIn.jsx
+++ src/pages/signIn/SignIn.jsx
@@ -43,11 +43,11 @@ export default function SignIn() {
 
       <div className="flex justify-center items-center my-5 gap-8">
         <div className="border-[1px] border-gray-300 rounded-md py-2 px-2">
-          <FaGoogle className="text-3xl text-blue-600 font-bold" />
+          <FaGoogle className="text-3xl  text-red-500  font-bold" />
         </div>
 
         <div className="border-[1px] border-gray-300 rounded-md py-2 px-2">
-          <FaFacebook className="text-3xl text-red-500 font-bold" />
+          <FaFacebook className="text-3xl text-blue-600 font-bold" />
         </div>
 
         <div className="border-[1px] border-gray-300 rounded-md py-2 px-2">
@@ -75,7 +75,7 @@ export default function SignIn() {
                   toast.error("Adresse e-mail invalide");
                   return false;
                 }
-              }
+              },
             })}
             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           />
@@ -103,7 +103,7 @@ export default function SignIn() {
                   toast.error("Mot de passe invalide");
                   return false;
                 }
-              }
+              },
             })}
             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           />
