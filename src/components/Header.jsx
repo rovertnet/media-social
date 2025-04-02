05e26224049e7f2d8cabc7197f9b25daba96d8ef
Robert Matundu <matundukabamba@gmail.com>
2025-04-02 22:47:46 +0100

    je suis un autre commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import motion  from "framer-motion";
+import { motion } from "framer-motion";
 import { Menu, X, Home, Info, Mail, User } from "lucide-react";
 
 const navItems = [
